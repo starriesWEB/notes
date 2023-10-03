90cb8c3a230918473a6a850437b5600c38bec655
starriesWEB <starries@aliyun.com>
2023-10-03 14:10:27 +0800

    add doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,30 +10,36 @@ module.exports = {
     title: "notes",
     description: '知识管理',
     head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-        ['link', {rel: 'icon', href: '/img/favicon.ico'}], //favicons，资源放在public文件夹
-        ['meta', {name: 'keywords', content: 'vuepress,theme,blog,vdoing'}],
-        ['meta', {name: 'theme-color', content: '#3eaf7c'}], // 移动浏览器主题颜色
+        ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+        ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }], // 移动浏览器主题颜色
+        ['meta', { name: 'referrer', content: 'no-referrer' }],
     ],
 
     // 主题配置
     themeConfig: {
         nav: [
-            {text: '首页', link: '/'},
-            {text: 'test', link: '/01.dir/03.test'},
+            { text: '首页', link: '/' },
             {
-                text: '测试',
-                // link: '/pages/13829e/',
-                items: [
-                    {text: 'HashSet源码分析', link: '/dir/HashSet源码分析/'},
-                    {text: 'Q&A.md', link: '/dir/Q&A/'},
-                    {text: 'test', link: '/dir/test/'},
-                ]
+                text: '读书笔记',
+                link: '/pages/af3d90/',
+                // items: [
+                //     {text: 'HashSet源码分析', link: '/dir/HashSet源码分析/'},
+                //     {text: 'Q&A.md', link: '/dir/Q&A/'},
+                //     {text: 'test', link: '/dir/test/'},
+                // ]
             },
-            {text: '赞助', link: '/pages/1b12ed/'},
+            { text: '系统设计', link: '/pages/afc736/' },
+            { text: '项目实战', link: '/pages/0eddfa/' },
+            { text: '学习笔记', link: '/pages/66d7bb/' },
+            { text: '知识学习', link: '/pages/cc712e/' },
+            { text: 'dir', link: '/pages/341814/' },
+            { text: '极客时间', link: 'https://geektime.pages.dev' },
+
         ],
         sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
         logo: '/img/logo.png', // 导航栏logo
-        repo: 'starriesWEB/geektime', // 导航栏右侧生成Github链接
+        repo: 'starriesWEB/notes', // 导航栏右侧生成Github链接
         searchMaxSuggestions: 10, // 搜索结果显示最大数
         lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
@@ -43,7 +49,7 @@ module.exports = {
 
         // 以下配置是Vdoing主题改动的和新增的配置
         // sidebar: {mode: 'structuring', collapsable: false}, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
-        sidebar: {mode: 'structuring', collapsable: false}, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+        sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
         sidebarOpen: true, // 初始状态是否打开侧边栏，默认true
         updateBar: { // 最近更新栏
@@ -122,7 +128,7 @@ module.exports = {
 
     markdown: {
         extendMarkdown: md => {
-            md.set({html: true});                    // required by mermaid
+            md.set({ html: true });                    // required by mermaid
         },
         extractHeaders: ['h2', 'h3', 'h4', 'h5', 'h6'], // 提取标题到侧边栏的级别，默认['h2', 'h3']
     },
